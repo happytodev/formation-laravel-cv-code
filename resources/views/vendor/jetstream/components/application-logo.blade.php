b685df65320562f1b30ef285c2ef1171376c6447
HappyToDev <frederic.blanc@itanea.fr>
2021-01-06 23:45:57 +0100

    Add HappyToDev Logo in svg

--- resources/views/vendor/jetstream/components/application-logo.blade.php
+++ resources/views/vendor/jetstream/components/application-logo.blade.php
@@ -1,5 +1,5 @@
 <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
- width="400.000000pt" height="400.000000pt" viewBox="0 0 400.000000 400.000000"
+ width="100.000000pt" height="100.000000pt" viewBox="0 0 400.000000 400.000000"
  preserveAspectRatio="xMidYMid meet">
 
 <g transform="translate(0.000000,400.000000) scale(0.100000,-0.100000)"
